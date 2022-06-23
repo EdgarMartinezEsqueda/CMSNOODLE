f73cafac1da1bbe25ae0918b56382ebd4a328674
EdgarMartinezEsqueda <71104237+EdgarMartinezEsqueda@users.noreply.github.com>
2022-06-23 16:59:32 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
-    host: "noodle.c27og4qpfmfg.us-east-1.rds.amazonaws.com",
+    host: "DIRECCIONDELADB",
     port: 3306,
-    user: "root",
-    password: "papanoelCISCO",
-    database: "Noodle"
+    user: "USUARIO",
+    password: "CONTRA",
+    database: "DB"
   }
 };
